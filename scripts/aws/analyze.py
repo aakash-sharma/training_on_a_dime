4f05c6beb5d4aaa8afe67eeaa2e925335e03110c
Aakash Sharma <asharma.com@gmail.com>
2021-11-07 13:12:46 -0500

    nov 7 changes

--- scripts/aws/analyze.py
+++ scripts/aws/analyze.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 import json
+import matplotlib.pyplot as plt
+
 from availability import instance_types
 
 TL = []
@@ -34,6 +36,11 @@ def instances_over_time(logs, logs2):
 
         i += 1
 
+
+    for instance in all_y_axis:
+        y_axis = all_y_axis[instance]
+        for i in range(len(y_axis)):
+            y_axis[i] = max(0, y_axis[i])
     
     print(all_y_axis)
 
@@ -43,6 +50,7 @@ def avg_lifespan(logs):
         lifespan_buckets[instance] = []
 
     end_time = datetime.strptime(TL[-1], '%Y-%m-%dT%H:%M:%S.000Z')
+    print(end_time)
 
     for instance_id in logs:
         instance = (logs[instance_id][0][0], logs[instance_id][0][1])
@@ -55,18 +63,42 @@ def avg_lifespan(logs):
         else:
             lifespan = end_time - st_time
 
+        if lifespan.total_seconds() < 0:
+            print(logs[instance_id])
+
 
         lifespan_buckets[instance].append(lifespan.total_seconds())
        
     print(lifespan_buckets) 
     
+def plot():
 
-        
+    #fig1, axs1 = plt.plot()    
+    instance = ("k80", 1)
+    y_axis = all_y_axis[instance]
+    plt.plot([i for i in range(len(TL))], y_axis)
+    plt.title("Spot instance (p2.xlarge) availability")
+    plt.xlabel('Normalized time interval')
+    plt.ylabel('Availability')
+
+    plt.show()
+
+    y_axis = lifespan_buckets[instance]
+    y_axis = [lifespan for lifespan in y_axis if lifespan > 0]
+    x_axis = [i for i in range(len(y_axis))]
+    plt.plot(x_axis, y_axis)
+    plt.title("Instance lifespan")
+    plt.ylabel('Seconds')
+    plt.xlabel('Normalized instance Id')
+    plt.show()
+
+
+    
 
 def main():
-    with open('dict_snapshot.bak', 'r') as dict_snapshot:
+    with open('dict_snapshot', 'r') as dict_snapshot:
         logs = json.load(dict_snapshot)
-    with open('dict_snapshot2.bak', 'r') as dict_snapshot2:
+    with open('dict_snapshot2', 'r') as dict_snapshot2:
         logs2 = json.load(dict_snapshot2)
 
 
@@ -74,9 +106,7 @@ def main():
     instances_over_time(logs, logs2)
     avg_lifespan(logs)
 
-    
-
-
+    plot()
 
 
 if __name__ == '__main__':
